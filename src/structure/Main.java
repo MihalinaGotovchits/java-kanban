bdafebae2a3417f1a3388133702961e1d5fe7b46
User Mihalina <Mix-Mihalina@yandex.ru>
2024-06-09 21:33:44 +0300

    Добавлены: новый класс и медоты для работы с файлами.

--- src/structure/Main.java
+++ src/structure/Main.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         File file = new File(String.valueOf(File.createTempFile("time", "file")));
 
         FileBackedTaskManager fileBackedTaskManager = new FileBackedTaskManager(file.toPath());
